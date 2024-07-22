5769769528b7b08196e5d135888f748f0273bf53
Geetha K S <140881546+GeethaSiddappa@users.noreply.github.com>
2024-07-22 15:23:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("my-topic");
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        ConsumeMessages(consumer, cts.Token);
+    }
+
+    static void ConsumeMessages(IConsumer<Ignore, string> consumer, CancellationToken token)
+    {
+        try
+        {
+            while (true)
+            {
+                try
+                {
+                    var consumeResult = consumer.Consume(token);
+                    Console.WriteLine($"Consumed message '{consumeResult.Value}' at: '{consumeResult.TopicPartitionOffset}'.");
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error occurred: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
